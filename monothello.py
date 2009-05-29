6c95761b5f06a3e06a08fd579fafd31fd2d062df
Ronald Kaiser <raios.catodicos@gmail.com>
2009-05-29 00:05:19 +0000

    Incluído mais um item de menu (About) e criada a classe Application

--- monothello.py
+++ monothello.py
@@ -1,21 +1,33 @@
 import sys
 from Tkinter import *
+import tkMessageBox
 
 
-def quit():
-    print "Thanks for playing MonOthello !"
-    sys.exit()
+class Application:
+    def __init__(self):
+
+        window = Tk()
+        window.title("MonOthello")
+
+        menu = Menu(window)
+        game_menu = Menu(menu, tearoff=0)
+        help_menu = Menu(menu, tearoff=0)
+        menu.add_cascade(label="Game", menu=game_menu)
+        menu.add_cascade(label="Help", menu=help_menu)
+        game_menu.add_command(label="Quit", command=self.bye)
+        help_menu.add_command(label="About", command=self.show_credits)
+
+        window.config(menu=menu)
+        window.mainloop()
+  
+    def show_credits(self):
+        message = "MonOthello\nv.: 1.0" 
+        tkMessageBox.showinfo(title="About", message=message)
+
+    def bye(self):
+        if tkMessageBox.askyesno(title="Quit", message="Really quit?"):
+            quit()
 
 
-window = Tk()
-window.title("MonOthello")
-
-menu = Menu(window)
-game_menu = Menu(menu, tearoff=0)
-game_menu.add_command(label="quit", command=quit)
-menu.add_cascade(label="Game", menu=game_menu)
-
-window.config(menu=menu)
-
-window.mainloop()
-
+if __name__ == "__main__":
+    app = Application()
