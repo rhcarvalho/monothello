7adc18e470e0b6de06b5ce93fda79761589483f4
Ronald Kaiser <raios.catodicos@gmail.com>
2009-05-29 21:10:08 +0000

    Adicionado novo item de menu (Settings), a opção para se criar um novo jogo (New -> game) e uma barra de status.

--- monothello.py
+++ monothello.py
@@ -5,19 +5,27 @@ import tkMessageBox
 
 class Application:
     def __init__(self):
-
+        
+        self.game = False
         window = Tk()
         window.title("MonOthello")
         window.wm_maxsize(width="400", height="400")
         window.wm_minsize(width="400", height="400")
 
         menu = Menu(window)
+
         game_menu = Menu(menu, tearoff=0)
+        settings_menu = Menu(menu, tearoff=0)
         help_menu = Menu(menu, tearoff=0)
+
         menu.add_cascade(label="Game", menu=game_menu)
+        menu.add_cascade(label="Settings", menu=settings_menu)
         menu.add_cascade(label="Help", menu=help_menu)
+
+        game_menu.add_command(label="New", command=self.create_game)
         game_menu.add_command(label="Quit", command=self.bye)
         help_menu.add_command(label="About", command=self.show_credits)
+
         window.config(menu=menu)
 
         back = Frame(window)
@@ -30,9 +38,19 @@ class Application:
                 button = Button(frame)
                 button["bg"] = "brown"
                 button.pack(side=LEFT, fill=BOTH, expand=1)
+        self.status = Label(window)
+        self.status["text"] = "Welcome to MonOthello!"
+        self.status.pack(side=LEFT)
         
         window.mainloop()
-  
+
+    def create_game(self):
+        if self.game:
+            if not tkMessageBox.askyesno(title="New", message="Are you sure you want to restart?"):
+                return 
+        self.game = Game()
+        self.status["text"] = "Game created."
+
     def show_credits(self):
         message = "MonOthello\nv.: 1.0" 
         tkMessageBox.showinfo(title="About", message=message)
@@ -40,6 +58,11 @@ class Application:
     def bye(self):
         if tkMessageBox.askyesno(title="Quit", message="Really quit?"):
             quit()
+
+
+class Game:
+    def __init__(self):
+        pass
 
 
 if __name__ == "__main__":
