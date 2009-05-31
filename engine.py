91347ddb14fb6fafef9c98024d39a7cdb013774d
Ronald Kaiser <raios.catodicos@gmail.com>
2009-05-31 13:54:02 +0000

    Refatoração do código e conserto de um bug no movimento de peças.

--- engine.py
+++ engine.py
@@ -1,6 +1,7 @@
 class Engine:
     def __init__(self, turn="B"):
-        """Put the pieces on the board and set the turn."""
+        """Put the pieces on the board, set the turn, scores and the directions 
+        to check valid positions."""
 
         self.board = dict()
         for row in range(8):
@@ -8,7 +9,10 @@ class Engine:
                 self.board[(row, column)] = "E"
         self.board[(3, 3)] = self.board[(4, 4)] = "B"
         self.board[(3, 4)] = self.board[(4, 3)] = "W"
-    
+
+        self.directions = [(1, 0), (0, 1), (-1, 0), (0, -1), 
+                           (1, 1), (-1, -1), (1, -1), (-1, 1)]
+  
         self.black_score = self.white_score = 2
         self.turn = turn
 
@@ -23,60 +27,46 @@ class Engine:
         If play is False, just check if the position is valid.
         """
 
-        row = position[0]
-        column = position[1]
-        change = list()
-
-        valid = 0
-
-        directions = [(1, 0), (0, 1), (-1, 0), (0, -1), 
-                      (1, 1), (-1, -1), (1, -1), (-1, 1)]
-
-        for direction in directions:            
-            i = row
-            j = column
-            count = 0
+        to_change = list()
+        any_valid_position = False
+        for direction in self.directions:
+            i, j = position
+            between = 0
                 
             while True:
-                if direction[0] == 1 and i > 6:
-                    break
-                if direction[0] == -1 and i < 1:
-                    break
-                if direction[1] == 1 and j > 6:
-                    break
-                if direction[1] == -1 and j < 1:
+                if (direction[0] == 1 and i == 7) or (direction[0] == -1 and i == 0) or \
+                   (direction[1] == 1 and j == 7) or (direction[1] == -1 and j == 0):
                     break
                 
                 i += direction[0]
                 j += direction[1]
 
-                if self.board[(i, j)] == self.turn and count == 0:
-                    break
-                elif self.board[(i, j)] == "E":
-                    break
-                elif self.turn == self.board[(i, j)] and count != 0:
-                    if not play:
-                        return True
-                    x = row
-                    y = column
-                    for times in range(count+1):
-                        change.append((x, y))
-                        x += direction[0]
-                        y += direction[1]
-                    valid += 1
+                if self.board[(i, j)] == "E":
                     break
                 elif self.turn != self.board[(i, j)]:
-                    count += 1    
+                    between += 1    
+                elif self.board[(i, j)] == self.turn:
+                    if between == 0:
+                        break
+                    else:
+                        any_valid_position = True
+                        if not play:
+                            return True
 
-        if valid:
-            if play:
-                for item in change:
-                    self.board[item] = self.turn
+                        x, y = position
+                        for times in range(between+1):
+                            to_change.append((x, y))
+                            x += direction[0]
+                            y += direction[1]
+                        break
 
-                self.change_turn()
+        if play and any_valid_position:
+            for item in to_change:
+                self.board[item] = self.turn
+
+            self.change_turn()
             return True
-        else:
-            return False
+        return False
 
     def find_valid_positions(self):
         """Return a list of valid positions."""
